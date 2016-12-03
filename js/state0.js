b08ea0587b53a3af534b1de9ac7e7bd30aca789d
PiotrMa <piotrmgrupa4@gmail.com>
2016-12-03 21:37:05 +0100

    Added animations and char

--- js/state0.js
+++ js/state0.js
@@ -1,15 +1,33 @@
 var demo = {}; //game object created 1 time
+
+var deidraChar;
+var speed = 4;
 //var upKey;
 demo.state0 = function(){};
 demo.state0.prototype = {
-	preload: function(){},
+	preload: function(){
+		//game.load.spritesheet('mummy', 'images/deidara.png', 90, 93, 12);
+		game.load.spritesheet('deidra',"images/deidra_walk1.png",51,60,12); //,
+	},
 	create: function(){
-		this.game.stage.backgroundColor = '#DDDDDD';
-		alert('state0');
+		this.game.stage.backgroundColor = '#800080';
+		//alert('state0');
 		//upKey = 
 		addEventStateListener();
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; //resizes the image to fit avaliable space keeping original ratio - global
+		deidraChar = game.add.sprite(game.world.centerX, game.world.centerY, 'deidra');
 		
-		
+		deidraChar.anchor.set(0.5);
+	    deidraChar.scale.set(2);
+	    deidraChar.smoothed = false;
+
+	    deidraChar.animations.add('Stand',[9]);
+	    deidraChar.animations.add('WalkRight',[0,1,2]);
+	    deidraChar.animations.add('WalkLeft',[3,4,5]);
+	    deidraChar.animations.add('WalkUp',[6,7,8]);
+	    deidraChar.animations.add('WalkDown',[9,10,11]);
+	    deidraChar.animations.play('Stand', 1, true);
+
 		//addKey(50,this.changeState,2);
 		//console.log(upKey);
 		//upKey.onDown.add(this.changeState, null, null, 1); //call changeState and pass it '1' as arg
@@ -22,8 +40,46 @@ demo.state0.prototype = {
 		this.game.state.start('state'+stateNum);
 	},*/
 
-	update: function(){
+	movementInput: function(){
+		if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+			console.log("RIGHT");
+			deidraChar.x+=speed;
+			deidraChar.animations.stop('Stand');
+			deidraChar.animations.play('WalkRight',12,true);
+		}
+		else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+			console.log("LEFT");
+			deidraChar.x-=speed;
+			deidraChar.animations.stop('Stand');
+			deidraChar.animations.play('WalkLeft',12,true);
+		}
+		else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+			console.log("UP");
+			deidraChar.y-=speed;
+			deidraChar.animations.stop('Stand');
+			deidraChar.animations.play('WalkUp',12,true);
+		}
+		else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+			console.log("UP");
+			deidraChar.y+=speed;
+			deidraChar.animations.stop('Stand');
+			deidraChar.animations.play('WalkDown',12,true);
+		}
+		else{
+			//console.log("STANDING");
+			deidraChar.animations.stop('Walk');
+			deidraChar.animations.play('Stand',1,true);
+		}
+	},
 
+	update: function(){
+		this.movementInput();
+		/*if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+			console.log("RIGHT");
+			deidraChar.x+=4;
+			deidraChar.animations.stop('Stand');
+			deidraChar.animations.play('Walk',6,true);
+		}*/
 	}
 };
 
@@ -42,3 +98,4 @@ function addEventStateListener(){
 			addKey(48+i,changeState,i);
 		}
 }
+
