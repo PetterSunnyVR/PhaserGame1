8117c574562e779ab80ba012e0d9e4d1a853ff70
PiotrMa <piotrmgrupa4@gmail.com>
2016-12-06 22:48:01 +0100

    state 5 completed

--- js/state4.js
+++ js/state4.js
@@ -1,8 +1,30 @@
+
+
 demo.state4 = function(){};
+
+var a1,a2,a3,a4,a5;
+var tween1;
+
 demo.state4.prototype = {
-	preload: function(){},
+	preload: function(){
+        game.load.spritesheet('deidra',"images/deidra_walk1.png",51,60,12); //,
+    },
 	create: function(){
 		addEventStateListener();
+        
+        a1 = game.add.sprite(50,100,'deidra');
+        a2 = game.add.sprite(350,100,'deidra');
+        a3 = game.add.sprite(650,100,'deidra');
+        a4 = game.add.sprite(950,100,'deidra');
+        a5 = game.add.sprite(1250,100,'deidra');
+        
+        
+        game.add.tween(a1).to({y: 400}, 2000, 'Linear', true);
+        tween1 = game.add.tween(a2).to({x: 100, y: 0}, 1000, 'Elastic.easeOut');
+        game.add.tween(a3).from({y: 1000}, 1500, 'Circ.easeOut', true);
+        game.add.tween(a4.anchor).to({x: 1}, 1000, 'Elastic.easeOut', true, 1000, 2 , true);
+        game.add.tween(a4.anchor).to({x: 1}, 1000, 'Elastic.easeOut', true, 1000, 2 , true);
+        game.add.tween(a5).to({alpha: 0}, 1000, 'Bounce', true, 100, 2 , true);
 	},
 	update: function(){
 
--- js/state5.js
+++ js/state5.js
@@ -1,10 +1,83 @@
 demo.state5 = function(){};
+var acc = 400;
+var platformGroup;
 demo.state5.prototype = {
-	preload: function(){},
+	preload: function(){
+        game.load.spritesheet('deidra',"images/deidra_walk1.png",51,60,12); //,
+        game.load.image('platform',"images/sand.png");
+    },
 	create: function(){
 		addEventStateListener();
-	},
+        
+        deidraChar = game.add.sprite(300, game.world.centerY+150, 'deidra');
+        platformGroup = game.add.group();
+        platform = game.add.sprite(0,800,'platform')
+        platform.scale.set(5);
+        platformGroup.create(650,400, 'platform');
+        platformGroup.create(1300,400, 'platform');
+       
+        game.physics.enable([deidraChar, platform, platformGroup]);
+        platformGroup.setAll('scale.x',3);
+        platformGroup.setAll('scale.y',3);
+        platformGroup.setAll('body.immovable',true);
+        //deidraChar.frame = 9;
+        deidraChar.body.gravity.y = 500;
+        deidraChar.body.bounce.y = 0.3;
+        deidraChar.body.collideWorldBounds = true;
+        deidraChar.anchor.set(0.5);
+        deidraChar.scale.set(2);
+        
+        deidraChar.animations.add('Stand',[9]);
+	    deidraChar.animations.add('WalkRight',[0,1,2]);
+	    deidraChar.animations.add('WalkLeft',[3,4,5]);
+	    deidraChar.animations.add('WalkUp',[6,7,8]);
+	    deidraChar.animations.add('WalkDown',[9,10,11]);
+	    //deidraChar.animations.play('Stand', 1, false);
+	    deidraChar.frame = 9;
+        deidraChar.body.drag.x = 400;
+        
+        platform.body.immovable = true;
+    },
 	update: function(){
-
+        this.movementInput();
+        game.physics.arcade.collide(deidraChar, [platform, platformGroup]);
+        //game.physics.arcade.collide(deidraChar, platformGroup);
+	},
+    movementInput: function(){
+		if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+			console.log("RIGHT");
+			deidraChar.body.acceleration.x = acc;
+			//deidraChar.animations.stop('Stand');
+			deidraChar.animations.play('WalkRight',12,true);
+		}
+		else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+			console.log("LEFT");
+			deidraChar.body.acceleration.x = -acc;
+			//deidraChar.animations.stop('Stand');
+			deidraChar.animations.play('WalkLeft',12,true);
+		}
+		else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+			console.log("UP");
+			deidraChar.body.velocity.y = -300;
+			//deidraChar.animations.stop('Stand');
+			deidraChar.animations.play('WalkUp',12,true);
+		}
+		else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+			console.log("UP");
+			deidraChar.y+=speed;
+			//deidraChar.animations.stop('Stand');
+			deidraChar.animations.play('WalkDown',12,true);
+		}
+		else{
+			//console.log("STANDING");
+			//deidraChar.animations.stop('Walk');
+			//deidraChar.animations.play('Stand',1,true);
+			deidraChar.frame = 9;
+            deidraChar.body.acceleration.x = 0;
+		}
+        if(deidraChar.x > 1464){
+            deidraChar.x = 1464;
+            deidraChar.body.acceleration.x = 0;
+        }
 	}
 };
